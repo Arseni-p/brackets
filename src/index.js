350bbcfba6a0a645835e71392c107053a0a5e46f
Arseni Pantsialei <arsenipantsialei@gmail.com>
2020-10-18 22:01:38 +0300

    init: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let strArr = str.split("");
+
+        for (let i = 0; i < strArr.length; i++) {
+
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                let configItem = bracketsConfig[j];
+                
+                if (strArr[i] === bracketsConfig[j][0] && strArr[i+1] === bracketsConfig[j][1]) {
+                    strArr.splice(i, 2);
+                    i = -1;
+                } 
+            }
+        }
+        return strArr.length === 0;
 }
